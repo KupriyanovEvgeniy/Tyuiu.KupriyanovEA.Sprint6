1dc69e734deb54c4ebf14771c7414e15596c39ec
Tyuiu.KupriyanovEA@outlook.com <Tyuiu.KupriyanovEA@outlook.com>
2023-12-02 13:41:48 +0500

    Task1

---
+++ Tyuiu.KupriyanovEA.Sprint6.Task1.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KupriyanovEA.Sprint6.Task1.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KupriyanovEA.Sprint6.Task1.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8bf43ce-5f91-45bd-87bd-06b581377dbf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KupriyanovEA.Sprint6.Task1.V23/FormMain.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace Tyuiu.KupriyanovEA.Sprint6.Task1.V23
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.groupBoxTask_KUE = new System.Windows.Forms.GroupBox();
+            this.pictureBoxFormula = new System.Windows.Forms.PictureBox();
+            this.textBoxUslovie = new System.Windows.Forms.TextBox();
+            this.groupBoxOutputData_KUE = new System.Windows.Forms.GroupBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.groupBoxInputData_KUE = new System.Windows.Forms.GroupBox();
+            this.labelInputStopStep_KUE = new System.Windows.Forms.Label();
+            this.labelInputStartStep_KUE = new System.Windows.Forms.Label();
+            this.textBoxInputStopStep_KUE = new System.Windows.Forms.TextBox();
+            this.textBoxInputStartStep_KUE = new System.Windows.Forms.TextBox();
+            this.buttonSpravka_KUE = new System.Windows.Forms.Button();
+            this.buttonStartCode_KUE = new System.Windows.Forms.Button();
+            this.groupBoxTask_KUE.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFormula)).BeginInit();
+            this.groupBoxOutputData_KUE.SuspendLayout();
+            this.groupBoxInputData_KUE.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxTask_KUE
+            // 
+            this.groupBoxTask_KUE.Controls.Add(this.pictureBoxFormula);
+            this.groupBoxTask_KUE.Controls.Add(this.textBoxUslovie);
+            this.groupBoxTask_KUE.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBoxTask_KUE.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxTask_KUE.Name = "groupBoxTask_KUE";
+            this.groupBoxTask_KUE.Size = new System.Drawing.Size(509, 338);
+            this.groupBoxTask_KUE.TabIndex = 0;
+            this.groupBoxTask_KUE.TabStop = false;
+            this.groupBoxTask_KUE.Text = "Условие";
+            // 
+            // pictureBoxFormula
+            // 
+            this.pictureBoxFormula.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxFormula.Image")));
+            this.pictureBoxFormula.Location = new System.Drawing.Point(6, 41);
+            this.pictureBoxFormula.Name = "pictureBoxFormula";
+            this.pictureBoxFormula.Size = new System.Drawing.Size(328, 46);
+            this.pictureBoxFormula.TabIndex = 1;
+            this.pictureBoxFormula.TabStop = false;
+            // 
+            // textBoxUslovie
+            // 
+            this.textBoxUslovie.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxUslovie.Location = new System.Drawing.Point(6, 21);
+            this.textBoxUslovie.Multiline = true;
+            this.textBoxUslovie.Name = "textBoxUslovie";
+            this.textBoxUslovie.ReadOnly = true;
+            this.textBoxUslovie.Size = new System.Drawing.Size(497, 282);
+            this.textBoxUslovie.TabIndex = 0;
+            this.textBoxUslovie.TabStop = false;
+            this.textBoxUslovie.Text = "Протабулироавать функцию:\r\n\r\n\r\n\r\n\r\n на заданном диапазоне. Результат вывести в ви" +
+    "де таблицы.";
+            // 
+            // groupBoxOutputData_KUE
+            // 
+            this.groupBoxOutputData_KUE.Controls.Add(this.textBoxResult);
+            this.groupBoxOutputData_KUE.Controls.Add(this.labelResult);
+            this.groupBoxOutputData_KUE.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBoxOutputData_KUE.Location = new System.Drawing.Point(528, 13);
+            this.groupBoxOutputData_KUE.Name = "groupBoxOutputData_KUE";
+            this.groupBoxOutputData_KUE.Size = new System.Drawing.Size(260, 425);
+            this.groupBoxOutputData_KUE.TabIndex = 1;
+            this.groupBoxOutputData_KUE.TabStop = false;
+            this.groupBoxOutputData_KUE.Text = "Вывод данных";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxResult.Location = new System.Drawing.Point(10, 41);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResult.Size = new System.Drawing.Size(244, 378);
+            this.textBoxResult.TabIndex = 1;
+            this.textBoxResult.TabStop = false;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(7, 22);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(68, 15);
+            this.labelResult.TabIndex = 0;
+            this.labelResult.Text = "Результат:";
+            // 
+            // groupBoxInputData_KUE
+            // 
+            this.groupBoxInputData_KUE.Controls.Add(this.labelInputStopStep_KUE);
+            this.groupBoxInputData_KUE.Controls.Add(this.labelInputStartStep_KUE);
+            this.groupBoxInputData_KUE.Controls.Add(this.textBoxInputStopStep_KUE);
+            this.groupBoxInputData_KUE.Controls.Add(this.textBoxInputStartStep_KUE);
+            this.groupBoxInputData_KUE.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBoxInputData_KUE.Location = new System.Drawing.Point(13, 357);
+            this.groupBoxInputData_KUE.Name = "groupBoxInputData_KUE";
+            this.groupBoxInputData_KUE.Size = new System.Drawing.Size(240, 81);
+            this.groupBoxInputData_KUE.TabIndex = 2;
+            this.groupBoxInputData_KUE.TabStop = false;
+            this.groupBoxInputData_KUE.Text = "Ввод данных";
+            // 
+            // labelInputStopStep_KUE
+            // 
+            this.labelInputStopStep_KUE.AutoSize = true;
+            this.labelInputStopStep_KUE.Location = new System.Drawing.Point(131, 35);
+            this.labelInputStopStep_KUE.Name = "labelInputStopStep_KUE";
+            this.labelInputStopStep_KUE.Size = new System.Drawing.Size(76, 15);
+            this.labelInputStopStep_KUE.TabIndex = 3;
+            this.labelInputStopStep_KUE.Text = "Конец шага:";
+            // 
+            // labelInputStartStep_KUE
+            // 
+            this.labelInputStartStep_KUE.AutoSize = true;
+            this.labelInputStartStep_KUE.Location = new System.Drawing.Point(6, 35);
+            this.labelInputStartStep_KUE.Name = "labelInputStartStep_KUE";
+            this.labelInputStartStep_KUE.Size = new System.Drawing.Size(75, 15);
+            this.labelInputStartStep_KUE.TabIndex = 2;
+            this.labelInputStartStep_KUE.Text = "Старт шага:";
+            // 
+            // textBoxInputStopStep_KUE
+            // 
+            this.textBoxInputStopStep_KUE.Location = new System.Drawing.Point(134, 53);
+            this.textBoxInputStopStep_KUE.Name = "textBoxInputStopStep_KUE";
+            this.textBoxInputStopStep_KUE.Size = new System.Drawing.Size(100, 22);
+            this.textBoxInputStopStep_KUE.TabIndex = 1;
+            // 
+            // textBoxInputStartStep_KUE
+            // 
+            this.textBoxInputStartStep_KUE.Location = new System.Drawing.Point(6, 53);
+            this.textBoxInputStartStep_KUE.Name = "textBoxInputStartStep_KUE";
+            this.textBoxInputStartStep_KUE.Size = new System.Drawing.Size(100, 22);
+            this.textBoxInputStartStep_KUE.TabIndex = 0;
+            // 
+            // buttonSpravka_KUE
+            // 
+            this.buttonSpravka_KUE.BackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonSpravka_KUE.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSpravka_KUE.Location = new System.Drawing.Point(274, 370);
+            this.buttonSpravka_KUE.Name = "buttonSpravka_KUE";
+            this.buttonSpravka_KUE.Size = new System.Drawing.Size(87, 62);
+            this.buttonSpravka_KUE.TabIndex = 3;
+            this.buttonSpravka_KUE.Text = "Справка";
+            this.buttonSpravka_KUE.UseVisualStyleBackColor = false;
+            this.buttonSpravka_KUE.Click += new System.EventHandler(this.buttonSpravka_KUE_Click);
+            // 
+            // buttonStartCode_KUE
+            // 
+            this.buttonStartCode_KUE.BackColor = System.Drawing.Color.Green;
+            this.buttonStartCode_KUE.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStartCode_KUE.Location = new System.Drawing.Point(367, 370);
+            this.buttonStartCode_KUE.Name = "buttonStartCode_KUE";
+            this.buttonStartCode_KUE.Size = new System.Drawing.Size(149, 62);
+            this.buttonStartCode_KUE.TabIndex = 4;
+            this.buttonStartCode_KUE.Text = "Выполнить";
+            this.buttonStartCode_KUE.UseVisualStyleBackColor = false;
+            this.buttonStartCode_KUE.Click += new System.EventHandler(this.buttonStartCode_KUE_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStartCode_KUE);
+            this.Controls.Add(this.buttonSpravka_KUE);
+            this.Controls.Add(this.groupBoxInputData_KUE);
+            this.Controls.Add(this.groupBoxOutputData_KUE);
+            this.Controls.Add(this.groupBoxTask_KUE);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Спринт 6 | Таск 1 | Вариант 23 | Куприянов Е.А. ";
+            this.groupBoxTask_KUE.ResumeLayout(false);
+            this.groupBoxTask_KUE.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFormula)).EndInit();
+            this.groupBoxOutputData_KUE.ResumeLayout(false);
+            this.groupBoxOutputData_KUE.PerformLayout();
+            this.groupBoxInputData_KUE.ResumeLayout(false);
+            this.groupBoxInputData_KUE.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxTask_KUE;
+        private System.Windows.Forms.TextBox textBoxUslovie;
+        private System.Windows.Forms.PictureBox pictureBoxFormula;
+        private System.Windows.Forms.GroupBox groupBoxOutputData_KUE;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.GroupBox groupBoxInputData_KUE;
+        private System.Windows.Forms.Label labelInputStopStep_KUE;
+        private System.Windows.Forms.Label labelInputStartStep_KUE;
+        private System.Windows.Forms.TextBox textBoxInputStopStep_KUE;
+        private System.Windows.Forms.TextBox textBoxInputStartStep_KUE;
+        private System.Windows.Forms.Button buttonSpravka_KUE;
+        private System.Windows.Forms.Button buttonStartCode_KUE;
+    }
+}
+
